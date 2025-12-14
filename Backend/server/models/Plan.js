5692181d361e154d798b3654418e42004f9e97d0
Anshul10107 <anshulsen10107@gmail.com>
2025-12-15 01:09:37 +0530

    Update Plan.js

--- Backend/server/models/Plan.js
+++ Backend/server/models/Plan.js
@@ -1,25 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const planSchema = new mongoose.Schema({
-//   title: String,
-//   description: String,
-//   price: Number,
-//   duration: String,
-//   trainer: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-// });
-
-// module.exports = mongoose.model("Plan", planSchema);
-
-
 const mongoose = require("mongoose");
 
 const planSchema = new mongoose.Schema({
   title: String,
   description: String,
-  level: String,
+  price: Number,
   duration: String,
-  trainer: String
+  trainerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  trainerName: String
 });
 
 module.exports = mongoose.model("Plan", planSchema);
-
